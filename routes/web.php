3f45a532ea27d1ce24f17c462c13794c09681a45
Walter Satriano <walter.satri@gmail.com>
2019-10-02 16:09:27 +0200

    definizione routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get("/placesIndex", "ControllerPlace@index") -> name("place.index");
+Route::get("/placeCreate", "ControllerPlace@create") -> name("place.create");
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post("/", "ControllerPlace@store") -> name("place.store");
